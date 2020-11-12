acbff82e9078544615629a4c77806f3904ca9bc5
sally al sardouk <sally.sardouk98@gmail.com>
2020-11-12 13:20:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const append =["MilO","Otis","Garfield"];
+const append =["Milo","Otis","Garfield"];
 const prepend =["Milo","Otis","Garfield"];
 const removeLast =["Milo","Otis","Garfield"];
 const removeFirst =["Milo","Otis","Garfield"];
