4c5933878497c3a0eee70ae7b1db780bae820057
sally al sardouk <sally.sardouk98@gmail.com>
2020-11-12 13:15:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append =["MILO","Otis","Garfield"];
+const prepend =["MILO","Otis","Garfield"];
+const removeLast =["MILO","Otis","Garfield"];
+const removeFirst =["MILO","Otis","Garfield"];
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
+
